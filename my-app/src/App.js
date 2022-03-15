87991f58e42a91afcb52130f1e63bb167e7c1d9b
venkatesh478 <mannemvenkatesh78@gmail.com>
2022-03-15 17:37:07 +0530

    add properIndentiation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
-import Button from "./Components/Button"
+import Button from "./Components/Button";
 
-const App=()=>{
-    const[count, setcount]=useState(0);
-    return(
-        <div className="App">
-            {count}
-            <Button type="button" onClickHandler={()=>setcount(count+1)} buttonText="submit"/>
-
-            </div>
-    )
-}
+const App = () => {
+  const [count, setcount] = useState(0);
+  return (
+    <div className="App">
+   <span>{count}</span>
+      <Button
+        type="button"
+        onClickHandler={() => setcount(count + 1)}
+        buttonText="submit"
+      />
+    </div>
+  );
+};
 export default App;
