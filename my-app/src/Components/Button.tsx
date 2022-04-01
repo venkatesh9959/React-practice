024822ae737914c782c9b8532c757e07f2f8f133
unknown <venki11478@gmail.com>
2022-04-01 09:46:37 +0530

    add chages

--- my-app/src/Components/Button.tsx
+++ my-app/src/Components/Button.tsx
@@ -6,19 +6,36 @@ interface ButtonProps {
   disable?: boolean;
   addclass?: string;
 }
+/**
+ * 
+ * @interface typeScript checking all values, and  the props definitions for Button Component
+ * @type propertie used for Button Type
+ * @addclasses propertie is used for  Button ClassName
+ * @value propertie is used for  Button value
+ * @onClickHandler properties is Button onClick  excute any function
+ * @disable properties is used Button disable 
+ * 
+ * @
+ */
 
 const Button = (props: ButtonProps) => {
   const { type, value, ButtonText, onClickHandler, disable, addclass } = props;
   return (
     <button
-      type={type || "button"} //Ikkada condtion rayala
+      type={type || "button"}
       value={value}
       onClick={onClickHandler}
       disabled={disable}
-      className={addclass}
+      className={`btn ${addclass && addclass}`}
     >
       {ButtonText}
     </button>
   );
 };
 export default Button;
+
+/**
+ *  Button  Component Is Used  for Button Fields
+ *  It used as Cancel  Button or Submit button or Reset button
+ * 
+ */
--- my-app/src/Components/InputText.tsx
+++ my-app/src/Components/InputText.tsx
@@ -8,12 +8,23 @@ interface Inputprops {
   value?: string;
   onChageHandler?: any;
 }
+/**
+ * 
+ * @interface typeScript checking all values, and  the props definitions for Button Component
+ * @type propertie used for Input filed Type
+ * @addclasses propertie is used for  Input filed ClassName
+ * @PlaceHolderText propertie is used for  Input filed placeHolder 
+ * @value properrtie is used for  Input filed value
+ * @onChagehandler properties is Input onchage function 
+ * 
+ * @
+ */
 const InputText = (props: Inputprops) => {
   const { type, addclasses, placeHolderText, value, onChageHandler } = props;
   return (
     <input
       type={type}
-      className={addclasses}
+      className={`btn ${addclasses&&addclasses}`}
       placeholder={placeHolderText}
       value={value}
       onChange={onChageHandler}
@@ -22,3 +33,10 @@ const InputText = (props: Inputprops) => {
 };
 
 export default InputText
+
+
+/**
+ *  InputText  Component Is Used  for the input fields 
+ *  It like password and gmail and text and number fields used resuble fields
+ * 
+ */
